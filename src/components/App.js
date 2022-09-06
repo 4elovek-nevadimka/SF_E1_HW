fffedbcfc8a185e792f167ba57aaf1eb298918c4
nevadimka <morozovvadimka@gmail.com>
2022-09-06 22:59:16 +0300

    Created app base

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import "../styles/App.css";
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+// import Header from "./Header";
+// import Main from "./Main";
+
+function App() {
+    return (
+        <>
+            {/* <Header buttonName={"My Button"} /> */}
+            {/* <Main /> */}
+            <h1>Hello Billy</h1>
+        </>
+    );
+}
+
+export default App;

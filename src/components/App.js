8e39ddde5f6171c821e2338d77a33e575c4849e6
nevadimka <morozovvadimka@gmail.com>
2022-09-07 21:36:29 +0300

    Added get geo

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import "../styles/App.css";
+import GetGeo from "./GetGeo";
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
 // import Header from "./Header";
@@ -12,6 +13,7 @@ function App() {
             {/* <Header buttonName={"My Button"} /> */}
             {/* <Main /> */}
             <h1>Hello Billy</h1>
+            <GetGeo />
         </>
     );
 }

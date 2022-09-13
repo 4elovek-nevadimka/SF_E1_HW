0d07504a78d32aba1dfab1bb45e5b3dc32eb57e6
nevadimka <morozovvadimka@gmail.com>
2022-09-13 20:19:35 +0300

    added city search results

--- src/components/GetCity.js
+++ src/components/GetCity.js
@@ -1,26 +1,70 @@
+import axios from "axios";
 import React, { useState } from "react";
-import { Button } from "react-bootstrap";
+import { Button, Table } from "react-bootstrap";
 import { Form, InputGroup } from "react-bootstrap";
+import { GetCityRequest } from "../hooks/useApiRequests";
 
 import "../styles/GetCity.css";
 
 function GetCity() {
 
+    let cityNameInput = React.createRef();
+
+    let [cities, setCities] = useState(null);
+
+    const findCity = () => {
+
+        console.log(cityNameInput.current.value);
+        const request = GetCityRequest(cityNameInput.current.value);
+        axios.get(request).then(
+            res => {
+                setCities(res.data);
+            },
+        );
+        console.log(JSON.stringify(cities));
+
+    };
+
     return (
-        <div className="component-container-left">
-            <div className="component-container-element">
-                <InputGroup className="mb-3">
-                    <Form.Control
-                        placeholder="Please enter the city..."
-                        aria-label="City for search"
-                        aria-describedby="inputGroup-sizing-default"
-                    />
-                </InputGroup>
+        <div>
+            <div className="component-container-left">
+                <div className="component-container-element">
+                    <InputGroup className="mb-3" id="city-name-input">
+                        <Form.Control
+                            placeholder="Please enter the city..."
+                            aria-label="City for search"
+                            aria-describedby="inputGroup-sizing-default"
+                            ref={cityNameInput}
+                        />
+                    </InputGroup>
+                </div>
+                <div className="component-container-element">
+                    <Button variant="secondary" onClick={findCity}>Search</Button>
+                </div>
             </div>
-            <div className="component-container-element">
-                <Button variant="secondary">Search</Button>
+            <div>
+                <div className="table-wrapper">
+                    <Table borderless hover size="sm" className="cities">
+                        <tbody>
+                            {
+                                cities != null ?
+                                    cities.map(city =>
+                                        <tr>
+                                            <td>
+                                                {city.name}, {city.country}
+                                            </td>
+                                            <td>
+                                                <small>[{city.lat}, {city.lon}]</small>
+                                            </td>
+                                        </tr>
+                                    ) : ""
+                            }
+                        </tbody>
+                    </Table>
+                </div>
             </div>
         </div>
+
     );
 }
 
